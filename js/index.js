25a3219baa985ba65896490ee7ebe9ce468f429d
Sam Dillenburg <39334877+hackbraten68@users.noreply.github.com>
2021-10-28 20:05:48 +0200

    nav bar toggle on/off works

--- js/index.js
+++ js/index.js
@@ -4,3 +4,11 @@ navToggle.addEventListener("click", () => {
     document.body.classList.toggle("nav-open")
 })
 
+const navLinks = document.querySelectorAll(".nav__link")
+
+navLinks.forEach(function (link) {
+        link.addEventListener('click', () => {
+            document.body.classList.remove('nav-open')
+            console.log("oh, hi - you´re checking out my code ;) ")
+        })
+    })
