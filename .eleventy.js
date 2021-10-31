6c8f3ce2b1ad9a7503848e30cf3ca192a73f57fa
Sam Dillenburg <39334877+hackbraten68@users.noreply.github.com>
2021-10-31 22:40:25 +0100

    11tyfied static pages

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,15 @@
+const { DateTime } = require("luxon")
+
 module.exports = function (eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy("./src/")
     eleventyConfig.addPassthroughCopy("./src/css/styles.css")
+
+    eleventyConfig.addFilter("postDate", (dateObj) => {
+        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED_WITH_WEEKDAY)
+    })
+
+    eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
 
     return {
         dir: {
--- public/blog/2nd_example.md
+++ public/blog/2nd_example.md
@@ -2,8 +2,8 @@
 title: My Second Article
 author: Sam Dillenburg
 date: 2021-05-01
-tags: ["post", "featured"]
-image: /assets/blog/article-1.jpg
+tags: ["post"]
+image: /img/devsam.png
 imageAlt: This is a test
 description: Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis accusantium sit illo neque rem omnis quaerat, nam similique vitae delectus ad magni vel quo maxime, magnam placeat. Reprehenderit, distinctio aliquam?
 ---
